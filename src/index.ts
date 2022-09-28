9608adeb173c834cc8f8ab4a3e80453eec1aac6b
yshir <yshir0020@gmail.com>
2022-09-28 12:19:04 +0900

    feat: add deploy.yml

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ export const helloWorld: HttpFunction = (req, res) => {
   res.status(200).send({
     ok: true,
     uuid: uuidv4(),
+    env1: process.env.VAL1 ?? 'not set',
+    env2: process.env.VAL2 ?? 'not set',
   });
 };
